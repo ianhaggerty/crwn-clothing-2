e86730edc3d1875f04e6ed03d8dba74cfada8722
Ian Haggerty <iahag001@yahoo.co.uk>
2019-12-02 19:21:20 +0000

    Broken home page intol smaller components. Finished styling menu-item component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import "./App.css";
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
+
+import "./App.scss";
 
 class App extends Component {
   render() {
